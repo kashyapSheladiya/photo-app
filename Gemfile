5a1634e7078fe5cfd35bf6db434298d7a8c807ee
kashyapSheladiya <kashyap27041993@gmail.com>
2019-05-06 15:21:34 +0200

    Setting authentication system - confirmation required using devise

--- Gemfile
+++ Gemfile
@@ -7,6 +7,10 @@ ruby '2.3.7'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
+gem 'devise'
+gem 'twitter-bootstrap-rails'
+gem 'devise-bootstrap-views'
+gem 'jquery-rails'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe WelcomeController, type: :controller do
     it 'returns a success response / root route test' do
       #get :index
       #response.successful?
-      expect(get: root_url(subdomain: nil)).to route_to(
+      #expect(get: root_url(subdomain: nil)).to ~> can also be used
+      expect(get: "/").to route_to(
         controller: "welcome",
         action: "index"
       )
